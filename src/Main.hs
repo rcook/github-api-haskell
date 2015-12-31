3fed2203a9a427ada97e092d128d0ea35718241f
Richard Cook <rcook@rcook.org>
2015-12-31 13:40:19 -0800

    Perform basic GitHub API request

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.ByteString
+import Data.Word (Word16)
+import Network.Http.Client
+import OpenSSL
+import System.IO.Streams (stdout)
+import qualified System.IO.Streams as Streams
+
+serverUrl :: ByteString
+serverUrl = "api.github.com"
+
+serverPort :: Word16
+serverPort = 443
+
+repoUrl :: ByteString
+repoUrl = "/users/rcook/repos"
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = withOpenSSL $ do
+  ctx <- baselineContextSSL
+  withConnection (openConnectionSSL ctx serverUrl serverPort) $ \c -> do
+    request <- buildRequest $ do
+      http GET repoUrl
+      setAccept "application/json"
+      setHeader "User-Agent" "rcookGitHubApiClient"
+    sendRequest c request emptyBody
+    receiveResponse c $ \_ i -> Streams.connect i stdout
+
