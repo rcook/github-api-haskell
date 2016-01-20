7ccdf598534a0ef232b75faa78cd6315bd54a0ea
Richard Cook <rcook@rcook.org>
2016-01-20 07:37:50 -0800

    Remove dependency on doctest-discover

--- test/Main.hs
+++ test/Main.hs
@@ -1,2 +1,15 @@
-{-# OPTIONS_GHC -F -pgmF doctest-discover #-}
+module Main (main) where
+
+import System.FilePath.Glob (glob)
+import Test.DocTest (doctest)
+
+includeDirs :: [String]
+includeDirs = []
+
+doctestWithIncludeDirs :: [String] -> IO ()
+doctestWithIncludeDirs fs = doctest (map ((++) "-I") includeDirs ++ fs)
+
+main :: IO ()
+main = do
+  glob "src/**/*.hs" >>= doctestWithIncludeDirs
 
